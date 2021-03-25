56589b43c62e948834d7dd6410c215841d29e636
mushfichowdhury <chowdhurymushfi@gmail.com>
2021-03-24 23:08:17 -0400

    changed bg

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,5 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components';
-import BGImage from './images/pixelcity.gif';
+import BGImage from './images/sky.jpeg';
 
 const colors = {
     primary: "black",
